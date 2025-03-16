32b0c57e8b0aea526b37b3e88597ec6abd75052d
amrElzohairy <aelzohairy9@gmail.com>
2025-03-16 17:36:08 +0200

    add drawer item

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dashboard/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -16,6 +17,8 @@ class CustomDrawer extends StatelessWidget {
             title: "John Doe",
             subtitle: "demon@gmail.com",
           ),
+          SizedBox(height: 10),
+          DrawerItemsListView(),
         ],
       ),
     );
