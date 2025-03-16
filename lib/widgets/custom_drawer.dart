5a40dc68a427499ab9b378713394a4b9a604b8a5
amrElzohairy <aelzohairy9@gmail.com>
2025-03-16 17:48:53 +0200

    add inactive items

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/widgets/active_and_inactive_item.dart';
 import 'package:responsive_dashboard/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dashboard/widgets/user_info_list_tile.dart';
 
@@ -19,6 +21,20 @@ class CustomDrawer extends StatelessWidget {
           ),
           SizedBox(height: 10),
           DrawerItemsListView(),
+          Expanded(child: SizedBox()),
+          InActiveDrawerItem(
+            drawerItemModel: DrawerItemModel(
+              title: "Settings",
+              image: Assets.imagesSettings,
+            ),
+          ),
+          InActiveDrawerItem(
+            drawerItemModel: DrawerItemModel(
+              title: "Logout",
+              image: Assets.imagesLogout,
+            ),
+          ),
+          const SizedBox(height: 48),
         ],
       ),
     );
